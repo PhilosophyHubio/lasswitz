d31b859b96e64ad3c6d0fc29c35daaf9142f41bf
Uriel Cruz Carmona <uriel@MacBook-Air-de-Uriel.local>
2024-03-12 12:08:22 -0600

    Conectando base

--- lasswitz/routes.py
+++ lasswitz/routes.py
@@ -2,3 +2,4 @@ def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('blank', '/_')
+    config.add_route('search', '/search')
--- lasswitz/views/manubot.py
+++ lasswitz/views/manubot.py
@@ -3,9 +3,45 @@ from pyramid.response import Response
 from sqlalchemy.exc import SQLAlchemyError
 import datetime
 import uuid
-
+from sqlalchemy.sql import select, and_, or_
 from .. import models
 
+from sqlalchemy import create_engine, MetaData, Table
+
+
+@view_config(route_name='search', renderer='lasswitz:templates/search_template.jinja2')
+def search_view(request):
+    try:
+        engine = create_engine('sqlite:////Users/uriel/Documents/lasswitz/zotero.sqlite')
+
+        # Crea un objeto MetaData y refleja las tablas de la base de datos de Zotero
+        metadata = MetaData()
+        
+        item_data_table = Table('itemData', metadata, autoload_with=engine)
+        item_data_values_table = Table('itemDataValues', metadata, autoload_with=engine)
+        items_table = Table('items', metadata, autoload_with=engine)
+        item_data_types_table = Table('itemTypes', metadata, autoload_with=engine)
+        select_stmt = (select(item_data_values_table.c.value)
+                       .join(item_data_table, item_data_values_table.c.valueID == item_data_table.c.valueID)
+                       .join(items_table, items_table.c.itemID == item_data_table.c.itemID)
+                       .join(item_data_types_table, items_table.c.itemTypeID == item_data_types_table.c.itemTypeID)
+                       .where(and_(
+                           or_(item_data_table.c.fieldID == 1, item_data_table.c.fieldID == 13),
+                           or_(
+                               items_table.c.itemTypeID == 7,
+                               items_table.c.itemTypeID == 8,
+                               items_table.c.itemTypeID == 22
+                           )
+                       )))
+        
+        connection = engine.connect()
+        result = connection.execute(select_stmt)
+
+        # Devolvemos los resultados a la plantilla
+        return {'results': result}
+    except SQLAlchemyError as e:
+        # Manejar errores de SQLAlchemy
+        return Response(f'Error de base de datos: {str(e)}', content_type='text/plain', status=500)
 
 @view_config(route_name='blank', renderer='lasswitz:templates/manubot.jinja2')
 def blank_view(request):
